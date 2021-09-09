85d88e56eefa397baf2bb045570b6a09541c07f6
diazdjul19 <diazdjul19@gmail.com>
2021-09-10 00:47:20 +0700

    fixbugs-offset 1.6

--- app/Http/Controllers/AbsentController.php
+++ app/Http/Controllers/AbsentController.php
@@ -268,12 +268,20 @@ class AbsentController extends Controller
         $dari_bln = $request->dari_bln."-01";
         $sampai_bln = $request->sampai_bln."-31";
 
+        // $data = MsAbsent::whereBetween('created_at',[$dari_bln, $sampai_bln])
+        // ->where('kelas_siswa',$request->kelas_siswa)
+        // ->where('th_pelajaran',$request->th_pelajaran)
+        // ->where('semester',$request->semester)
+        // ->select(DB::raw("id, nama_siswa, nis_siswa, kelas_siswa,  SUM(jml_sakit_bln) as sakit, SUM(jml_izin_bln) as izin, SUM(jml_alpa_bln) as alpa"))
+        // ->groupBy('nis_siswa')
+        // ->get(); 
+
         $data = MsAbsent::whereBetween('created_at',[$dari_bln, $sampai_bln])
         ->where('kelas_siswa',$request->kelas_siswa)
         ->where('th_pelajaran',$request->th_pelajaran)
         ->where('semester',$request->semester)
         ->select(DB::raw("id, nama_siswa, nis_siswa, kelas_siswa,  SUM(jml_sakit_bln) as sakit, SUM(jml_izin_bln) as izin, SUM(jml_alpa_bln) as alpa"))
-        ->groupBy('nis_siswa')
+        ->group('nis_siswa')
         ->get(); 
 
         return view('dashboard_admin.absent.rekapulasi_absent', compact('data_url', 'kelas', 'get_kelas_siswa','get_semester_active', 'get_th_pelajaran' ,    'data', 'dari_bln', 'sampai_bln'));
